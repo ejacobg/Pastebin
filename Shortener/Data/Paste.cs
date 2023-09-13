30cf062c612e2ff0e57558814a345a45fcdff27e
Ean Jacob Gayban <ejacobg@outlook.com>
2023-09-13 16:17:14 -0700

    Added initial Paste model

---
+++ Shortener/Data/Paste.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shortener.Data
+{
+    public class PasteContext : DbContext
+    {
+        public DbSet<Paste> Pastes { get; set; }
+
+        public PasteContext(DbContextOptions<PasteContext> options)
+            : base(options)
+        {
+        }
+    }
+
+    [Index(nameof(Shortlink), IsUnique = true)]
+    public class Paste
+    {
+        [Key] public int PasteId { get; set; }
+
+        // Apply length constraints?
+        public string Shortlink { get; set; }
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)] public DateTime Created { get; set; }
+        public int Expires { get; set; } // Expiration in minutes after creation.
+    }
+}
