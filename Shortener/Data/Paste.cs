cfc2de14e374410551bf25694f0daf7eacc9ef4c
Ean Jacob Gayban <ejacobg@outlook.com>
2023-09-17 12:11:28 -0700

    Created and ran migrations

--- Shortener/Data/Paste.cs
+++ Shortener/Data/Paste.cs
@@ -17,13 +17,16 @@ namespace Shortener.Data
     [Index(nameof(Shortlink), IsUnique = true)]
     public class Paste
     {
-        [Key] public int PasteId { get; set; }
+        [Key]
+        public int PasteId { get; set; }
 
         // Apply length constraints?
         public string Shortlink { get; set; }
         public string Content { get; set; }
 
-        [DataType(DataType.DateTime)] public DateTime Created { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Created { get; set; }
+
         public int Expires { get; set; } // Expiration in minutes after creation.
     }
 }
---
+++ Shortener/Migrations/20230917190723_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shortener.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pastes",
+                columns: table => new
+                {
+                    PasteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Shortlink = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Expires = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pastes", x => x.PasteId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pastes_Shortlink",
+                table: "Pastes",
+                column: "Shortlink",
+                unique: true,
+                filter: "[Shortlink] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pastes");
+        }
+    }
+}
