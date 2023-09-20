ddb0b6a120ab32a9407e001dd21d22e970f70cd3
Ean Jacob Gayban <ejacobg@outlook.com>
2023-09-19 21:30:23 -0700

    Added lengthener service

---
+++ Lengthener/Controllers/LengthenController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Lengthener.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lengthener.Controllers
+{
+    [ApiController]
+    public class LengthenController : ControllerBase
+    {
+        private readonly IPasteStore _pasteStore;
+
+        public LengthenController(IPasteStore pasteStore)
+        {
+            _pasteStore = pasteStore;
+        }
+
+        [HttpGet("/lengthen/{shortlink}")]
+        public async Task<ActionResult<LengthenResult>> Lengthen(string shortlink)
+        {
+            var paste = await _pasteStore.GetByShortlink(shortlink);
+            if (paste is null)
+            {
+                return NotFound();
+            }
+
+            return new LengthenResult(
+                paste.Content,
+                paste.Created.ToString("yyyy-MM-dd hh:mm:ss"),
+                paste.Expires
+            );
+        }
+
+        public record LengthenResult(string Content, string Created, int Expires);
+    }
+}
---
+++ Lengthener/Models/PasteStore.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shortener.Data;
+
+namespace Lengthener.Models
+{
+    public interface IPasteStore
+    {
+        public Task<Paste> GetByShortlink(string shortlink);
+    }
+
+    public class SqlServerStore : IPasteStore
+    {
+        private readonly PasteContext _context;
+
+        public SqlServerStore(PasteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Paste> GetByShortlink(string shortlink)
+        {
+            return await _context.Pastes
+                .Where(paste => paste.Shortlink == shortlink)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
