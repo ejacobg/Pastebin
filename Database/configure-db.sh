81991ec7c3c8a7a21095c847d16d1c62ce8f4cb3
Ean Jacob Gayban <ejacobg@outlook.com>
2023-09-29 23:56:52 -0700

    Applied logic changes from pull requests

--- Database/configure-db.sh
+++ Database/configure-db.sh
@@ -11,8 +11,8 @@ DBSTATUS=1
 ERRCODE=1
 i=0
 
-while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
+while [[ $DBSTATUS -ne 0 || $ERRCODE -ne 0 ]] && [[ $i -lt 60 ]]; do
+	i=$(($i+1))
 	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $MSSQL_SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
 	ERRCODE=$?
 	sleep 1
